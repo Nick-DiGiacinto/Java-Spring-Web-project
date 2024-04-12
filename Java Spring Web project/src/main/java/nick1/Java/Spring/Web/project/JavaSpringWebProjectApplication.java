524f1901eb06e79009ef41e190df9419934967e2
Nick-DiGiacinto <151628711+Nick-DiGiacinto@users.noreply.github.com>
2024-04-13 00:29:54 +0200

    Project initialized

---
+++ Java Spring Web project/src/main/java/nick1/Java/Spring/Web/project/JavaSpringWebProjectApplication.java
@@ -0,0 +1,13 @@
+package nick1.Java.Spring.Web.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWebProjectApplication.class, args);
+	}
+
+}
---
+++ Java Spring Web project/src/test/java/nick1/Java/Spring/Web/project/JavaSpringWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nick1.Java.Spring.Web.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
